dba275826dad24b032f491cfe3b21d534ef66bb8
Priyaranjan kumar <59186153+Priyaranjan03@users.noreply.github.com>
2023-10-17 12:50:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+
 streamlit.title('Hello! i just started learning')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
