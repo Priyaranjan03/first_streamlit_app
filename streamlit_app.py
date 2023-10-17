ad03244a33a367ba082cbe81e038e38be4458d3b
Priyaranjan kumar <59186153+Priyaranjan03@users.noreply.github.com>
2023-10-17 13:00:59 +0530

    Update streamlit_app.py
    use multi select

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,8 @@ streamlit.text('🥗 🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header('List of Fruits')
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 
