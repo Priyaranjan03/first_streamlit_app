a9d0ac71247ee735a5e9b97d810755b014ca1a47
Priyaranjan kumar <59186153+Priyaranjan03@users.noreply.github.com>
2023-10-17 13:13:31 +0530

    Update streamlit_app.py
    add fruits sorted or filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ streamlit.header('List of Fruits')
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index)
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
-streamlit.dataframe(my_fruit_list)
+fruits_selected=streamlit.multiselect("Pick some fruits:",list(my_fruit.index)),['Avacodo','strawberries'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
 
