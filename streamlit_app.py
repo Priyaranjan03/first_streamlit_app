1d4f0bdfc0b9fac4ab0384c06ecafcf7d5bfb1b3
Priyaranjan kumar <59186153+Priyaranjan03@users.noreply.github.com>
2023-10-18 19:28:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,11 @@
 import streamlit
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.title('Hello! i just started learning')
+streamlit.title('My Parent New Healthy Diner')
 
 streamlit.header('🥣 🥗 Breakfast Menu 🥑🍞')
 streamlit.text('🐔 🥑🍞Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🍞Avocado Toast')
 
-streamlit.header('List of Fruits')
-
-my_fruit_list = my_fruit_list.set_index('Fruit')
-#Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-#fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avacodo','strawberries'])
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(my_fruit_list)
-
-streamlit.header("Fruityvice Fruit Advice!")
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-import snowflake.connector
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-streamlit.text(fruityvice_response.json())
-
-
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.header("The Fruit load list contains:")
-streamlit.dataframe(my_data_row)
-
-#streamlit.write('Thanks for adding',add_my_fruit)
-
-#This will not work properly
-my_cur.excecute("insert into fruit_load_list values('from streamlit')")
-
+streamlit.header('Build your own fruit smoothie 🥣')
